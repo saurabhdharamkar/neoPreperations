fc89e346799ad5b14005a948edf04f027b2188f7
Neosoft <Neosoft@192.168.1.53>
2023-08-01 16:44:44 +0530

    Commited

---
+++ Factory/src/com/neo/Shape.java
@@ -0,0 +1,6 @@
+package com.neo;
+
+public interface Shape {
+	void draw();
+
+}
---
+++ Factory/src/com/neo/Square.java
@@ -0,0 +1,11 @@
+package com.neo;
+
+public class Square implements Shape{
+
+	@Override
+	public void draw() {
+		System.out.println("draw Squares");
+		
+	}
+
+}
